318d01bd15c4af40b2af26bbf5d2402063583e14
Felix Breidenstein <mail@felixbreidenstein.de>
2014-09-25 12:55:18 +0200

    New js code for converting hsv to rgb and changed the slider from [0,255] to [0,1]

--- static/slider.js
+++ static/slider.js
@@ -5,31 +5,26 @@ function httpGet(theUrl){
 	xmlHttp.send( null );
 };
 
-function hslToRgb(h, s, l){
-	var r, g, b;
+function hsvToRgb(h, s, v){
+    var r, g, b;
 
-	if(s == 0){
-			r = g = b = l; // achromatic
-		}else{
-			function hue2rgb(p, q, t){
-				if(t < 0) t += 1;
-				if(t > 1) t -= 1;
-				if(t < 1/6) return p + (q - p) * 6 * t;
-				if(t < 1/2) return q;
-				if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-				return p;
-			}
-	
-				var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-				var p = 2 * l - q;
-				r = hue2rgb(p, q, h + 1/3);
-				g = hue2rgb(p, q, h);
-				b = hue2rgb(p, q, h - 1/3);
-			}
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
 
-	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    switch(i % 6){
+	        case 0: r = v, g = t, b = p; break;
+	        case 1: r = q, g = v, b = p; break;
+	        case 2: r = p, g = v, b = t; break;
+	        case 3: r = p, g = q, b = v; break;
+	        case 4: r = t, g = p, b = v; break;
+	        case 5: r = v, g = p, b = q; break;
+	    }
+
+    return [r * 255, g * 255, b * 255];
 }
-
 
 function updateSlider() {
 	var slider1 = document.getElementById("slider1");
@@ -40,9 +35,13 @@ function updateSlider() {
 	s = slider2.value;
 	v = slider3.value;
 	
-	rgb = hslToRgb(h,s,v)
+	rgb = hsvToRgb(h,s,v);
 
-//	httpGet("http://192.168.23.117:5000/send/red/"+rgb[0]);
-//	httpGet("http://192.168.23.117:5000/send/green/"+rgb[1]);
-//	httpGet("http://192.168.23.117:5000/send/blue/"+rgb[2]);	
+	r = Math.floor(rgb[0])
+	g = Math.floor(rgb[1])
+	b = Math.floor(rgb[2])
+
+	httpGet("http://192.168.23.117:5000/send/red/"+r);
+	httpGet("http://192.168.23.117:5000/send/green/"+g);
+	httpGet("http://192.168.23.117:5000/send/blue/"+b);	
 }
